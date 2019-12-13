ca9f0b2118b21dbe2a6af72a92dd851590ec0c9a
Chayan Martinez <nayachmartinez68@gmail.com>
2019-12-13 17:50:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,19 +1,1 @@
 def speak_to_grandma
-  print "Say somthing to grandma :"
-  user_input = gets.chomp!
-  user_input.downcase
-  user_input.upcase
-  
-
-if user_input.downcase
-  print " HUH?! SPEAK UP, SONNY!"
-  
-elsif user_input == "I LOVE YOU GRANDMA!"
-  print "I LOVE YOU TOO PUMPKIN!"
-  
-else user_input == "Joe"
-  print "NOT SINCE 1938!"
-  
-end
-end
-speak_to_grandma
