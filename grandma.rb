120931a2e8c6202432a783f0bc8c2cc565c361ab
Chayan Martinez <nayachmartinez68@gmail.com>
2019-12-13 17:56:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ def speak_to_grandma
   if  user_input == "I LOVE YOU GRANDMA!"
     print "I LOVE YOU TOO PUMPKIN!"
   
-  elsif user_input.upcase
+  elsif user_input = gets.upcase
   print "NO, NOT SINCE 1938!"
   
   else user_input
