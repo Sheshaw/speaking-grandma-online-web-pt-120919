69d4b1e8d504658899abd021dd33ebc4b6424d69
Chayan Martinez <nayachmartinez68@gmail.com>
2019-12-13 17:53:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,2 +1,14 @@
 def speak_to_grandma(user_input = gets.chomp!)
+  print " Say something to Grandma : "
   
+  if  user_input == "I LOVE YOU GRANDMA!"
+    print "I LOVE YOU TOO PUMPKIN!"
+  
+  elsif user_input.upcase
+  print "NO, NOT SINCE 1938!"
+  
+  else user_input
+    print "HUH?! SPEAK UP, SONNY!"
+    
+  end
+end
